9014f45d3bb7f3d8950892a56708ad4e735613e4
Siddharth Bhal <sidd.bhal@gmail.com>
2016-07-10 06:03:33 -0700

    insertionSort using rotate and upper_bound

--- c_cpp_mixed_algos/insertionSort.cpp
+++ c_cpp_mixed_algos/insertionSort.cpp
@@ -14,8 +14,15 @@ void insertionSort(std::vector<int>& V){
         V[j+1] = temp;
     }
 }
+void insertionSort(std::vector<int>& V) {
+	auto start = V.begin();
+	auto end = V.end();
+	// Rotate(first, middle, last)- takes a range [first, last) and rotates it so that the middle element becomes the first in that range.
+	//upper_bound - Returns an iterator pointing to the first element in the range [first,last) which compares greater than val. The range should be already sorted (or at least partitioned).
+	for (auto i = start; i != end; ++i)
+		std::rotate(std::upper_bound(start, i, *i), i, std::next(i));
+}
 
- 
 int main() {
   std::vector<int> V = {100, 2, 56, 200, -52, 3, 99, 33, 177, -199};
   insertionSort(V);
